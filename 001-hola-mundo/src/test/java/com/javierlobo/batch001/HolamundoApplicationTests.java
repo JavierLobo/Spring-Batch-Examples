e48f559fcd64dfe34f207c047631bef360bd754c
Javier Lobo <franciscojavier.lobo@gmail.com>
2023-02-02 12:50:53 +0100

    #1 Añadido el codigo necesario para solventar la issue. Close

---
+++ 001-hola-mundo/src/test/java/com/javierlobo/batch001/HolamundoApplicationTests.java
@@ -0,0 +1,14 @@
+// package com.javierlobo.batch001;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class Batch001ApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 		System.out.println("Hola Mundo");
+// 	}
+
+// }
